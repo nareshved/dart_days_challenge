77ace7030c7df12eb60edd95be7fe469fb306b0b
Naresh Kumar Vaid <nkraj021@gmail.com>
2024-12-30 14:26:41 +0530

    started dart days challenges

---
+++ bin/dart_days_challenge.dart
@@ -0,0 +1,6 @@
+// import 'package:dart_days_challenge/dart_days_challenge.dart'
+//     as dart_days_challenge;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${dart_days_challenge.calculate()}!');
+// }
---
+++ test/dart_days_challenge_test.dart
@@ -0,0 +1,8 @@
+// import 'package:dart_days_challenge/dart_days_challenge.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
